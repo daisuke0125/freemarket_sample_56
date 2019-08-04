344876d98508168ed443c3ab29117bc0a76ed5e1
daisuke0125 <daisukeno19840125@gmail.com>
2019-08-04 14:32:51 +0900

    ・テストコードの雛形作成
    ・validatesの記載
    ・カラム追加
    ・テストコード雛形追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :nickname, presence: true
+  validates :number, presence: true, uniqueness: true
+  validates :birthday, presence: true
+  validates :postCode, presence: true
+  validates :prefectures, presence: true
+  validates :city, presence: true
+  validates :streetNumber, presence: true
+  validates :cordNumber, presence: true, uniqueness: true
+  
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_034457) do
+ActiveRecord::Schema.define(version: 2019_08_04_051245) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_08_04_034457) do
     t.text "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "name_kana", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
