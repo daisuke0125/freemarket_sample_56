732853fc73efdbbe2a120a01230087c650c0c750
key2-hue <keymaeda3782@gmail.com>
2019-08-11 17:18:22 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2019_08_08_080929) do
-=======
-ActiveRecord::Schema.define(version: 2019_08_11_073327) do
->>>>>>> Stashed changes
+ActiveRecord::Schema.define(version: 2019_08_11_071053) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_number", null: false
+    t.integer "exp_month", null: false
+    t.integer "exp_year", null: false
+    t.integer "cvc", null: false
     t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "card_number"
-    t.integer "exp_month"
-    t.integer "exp_year"
-    t.integer "cvc"
     t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name", null: false
-    t.text "detail", null: false
-    t.string "image", null: false
-    t.text "brand"
-    t.string "condition", null: false
-    t.string "delivery", null: false
-    t.string "area", null: false
-    t.string "days", null: false
-    t.integer "price", null: false
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_08_11_073327) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
     t.integer "number", null: false
     t.integer "birthday", null: false
     t.integer "postcode", null: false
@@ -70,14 +49,12 @@ ActiveRecord::Schema.define(version: 2019_08_11_073327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname", null: false
-    t.string "name_kana", null: false
-    t.string "uid"
-    t.string "provider"
     t.string "location"
     t.string "image"
-    t.integer "cardNumber"
-    t.integer "expirationDate"
-    t.integer "securityCode"
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
