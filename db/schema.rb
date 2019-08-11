ec5445a7014c5f9de52d3109a58628293abb3ba9
daisuke0125 <daisukeno19840125@gmail.com>
2019-08-11 16:16:15 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_074041) do
+ActiveRecord::Schema.define(version: 2019_08_11_071053) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "card_number", null: false
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 2019_08_08_074041) do
     t.string "nickname", null: false
     t.string "location"
     t.string "image"
-    t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
