4520e73d9d5e5cd645a42500facbe1e5c2cdab7f
key2-hue <keymaeda3782@gmail.com>
2019-08-11 16:49:25 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_08_08_080929) do
+=======
+ActiveRecord::Schema.define(version: 2019_08_11_073327) do
+>>>>>>> Stashed changes
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
