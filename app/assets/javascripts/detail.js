0840251926ddbf4d59aa302e2b50887326a61cb3
key2-hue <keymaeda3782@gmail.com>
2019-08-12 14:10:32 +0900

    commit

--- app/assets/javascripts/detail.js
+++ app/assets/javascripts/detail.js
@@ -5,6 +5,8 @@ $(document).on('turbolinks:load',function(){
     draggable: false,
     // variableWidth: true,
     swipeToSlide: false,
+    // cssEase: 'linear',
+    initialSlide: 0,
   });
   // $('.thumbnail-thumb').slick({
   //   infinite: true,
@@ -23,9 +25,33 @@ $(document).on('turbolinks:load',function(){
     variableWidth: true,
     pauseOnFocus: false,
   });
+  // $('.thumbnail-thumb__photo').on('mouseover', function(){
+  //   var photo = $(this).attr('src');
+  //   var distance = $('.thumbnail-thumb__photo').index(this);
+  //   var ml = $(this).offset();
+  //   console.log(ml.left);
+  //   var topDistance = $('.thumbnail-image').attr('src', photo );
+  //   console.log(topDistance);
+  //   var topLeft = topDistance.offset();
+  //   topDistance.animate({left: topLeft.left},200);
+  //   $(this).css('opacity', '1.0');
+  //   $('.thumbnail-thumb__photo').not(this).css('opacity','0.5');
+  // });
   $('.thumbnail-thumb__photo').on('mouseover', function(){
     var photo = $(this).attr('src');
-    $('.thumbnail-image').attr('src', photo );
+    var topPhoto = $('.thumbnail-image').attr(photo);
+    var distance = $('.thumbnail-thumb__photo').index(this);
+    var ml = $(this).offset();
+    // console.log(slick.$slides.eq(index).find('img').attr('src'));
+    // console.log()
+    // var topDistance = $('.thumbnail-image').attr('src',photo);
+    // $('.thumbnail-image').css('transform', 'translate(photo.left,0px)');
+    // $('.thumbnail-image').stop().animate({'marginLeft': });
+    // console.log(topDistance);
+    // var topLeft = topDistance.offset();
+    $('.thumbnail-image').animate({marginLeft : '300px' },200);
     $(this).css('opacity', '1.0');
+    $('.thumbnail-thumb__photo').not(this).css('opacity','0.5');
+    console.log($(this).scrollLeft());
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -25,3 +25,5 @@ Rails.application.routes.draw do
     end  
   end
 
+  get "users/detail" => "items#detail"
+end
