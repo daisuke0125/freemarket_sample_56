3d8291ada195fd4efb2f0b7d8a828e93b5c026ca
key2-hue <keymaeda3782@gmail.com>
2019-08-16 17:04:15 +0900

    third commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   belongs_to user, foreign_key: 'user_id'
   belongs_to :category
 
+  has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images
+
 end
