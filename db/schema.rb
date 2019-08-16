5c2f270e902a747f04ff232f67db78f23567b480
key2-hue <keymaeda3782@gmail.com>
2019-08-16 17:01:19 +0900

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_083747) do
+ActiveRecord::Schema.define(version: 2019_08_16_075939) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "card_number", null: false
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 2019_08_14_083747) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_goods_on_item_id"
     t.index ["user_id"], name: "index_goods_on_user_id"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "images"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
