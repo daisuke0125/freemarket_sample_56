45fa9a323c43d186d5a5530424a4fc1ab15851c1
key2-hue <keymaeda3782@gmail.com>
2019-08-21 16:03:33 +0900

    second commit 8/21

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -19,7 +19,7 @@ $(document).on('turbolinks:load', function(){
     inputs.push($(this));
     var img = $(`<div class= "img_view"><img></div>`);
     reader.onload = function(e) {
-      var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
+      var btn_wrapper = $('<div class="btn_wrapper-image"><div class="btn-edit">編集</div><div class="btn-delete">削除</div></div>');
       img.append(btn_wrapper);
       img.find('img').attr({
         src: e.target.result
@@ -27,21 +27,23 @@ $(document).on('turbolinks:load', function(){
     }
     reader.readAsDataURL(file);
     images.push(img);
-
+    if(images.length >= 5) {
+      $('.sell-form__upload').css({'height': '540px'})
     if(images.length > 5) {
-      dropzone2.css({
+      box.css({
         'display': 'block'
       })
       $.each(images, function(index, image) {
         image.attr('data-image', index);
         preview2.append(image);
-        dropzone2.css({
+        $('#dropzone2').css({
           'width': `calc(100% - (135px * ${images.length - 5}))`
         })
       })
       if(images.length == 9) {
         box.find('p').replaceWith('<i class="fa fa-camera"></i>')
       }
+    } 
     } else {
         $('#preview').empty();
         $.each(images, function(index, image) {
@@ -49,12 +51,12 @@ $(document).on('turbolinks:load', function(){
           preview.append(image);
         })
         box.css({
-          'width': `calc(100% - (135px * ${images.length}))`,
+          'width': `calc(100% - (130px * ${images.length}))`,
           'left': `calc(130px * ${images.length})` 
         })
         preview.css ({
-          'left': `calc(-185px * ${images.length})`,
-          'marginLeft': `calc(70px * ${images.length - 1})`
+          'left': `calc(-160px * ${images.length})`,
+          'marginLeft': `calc(35px * ${images.length - 1})`
         })
         upload.css ({
           'width': `calc(622px - (135px * ${images.length}))`,
