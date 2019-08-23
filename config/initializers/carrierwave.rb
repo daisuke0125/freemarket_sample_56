f19b1276224eb4ba055bd2cf2bda59c60535fdde
KotaMorikawa <koke.kokota@gmail.com>
2019-08-23 21:17:13 +0900

    aaa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,17 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1' 
-  }
-
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1' 
+    }
+  end
   config.fog_directory  = 'freemarket-sample56'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample56'
 end
