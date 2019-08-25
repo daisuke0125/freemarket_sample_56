673173b8fc5608a0f3007568353f026a3ae1d415
kanji0028 <bukurabo@gmail.com>
2019-08-25 14:58:38 +0900

    space 2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,104 +1,103 @@
 class ItemsController < ApplicationController
 
-    def index
-        @items = Item.all
-        @image = []
-        @items.each do |item|
-            item.images.each_with_index do |p,index|
-                @image << p
-            end
-        end
-        @images = @image
+  def index
+    @items = Item.all
+    @image = []
+    @items.each do |item|
+      item.images.each_with_index do |p,index|
+        @image << p
+      end
+    end
+    @images = @image
+  end
 
-    end
+  def mypage
+    user = User.find(params[:id])
+    @nickname = user.nickname
+  end
 
-    def mypage
-      user = User.find(params[:id])
-      @nickname = user.nickname
-    end
+  def edit 
+  end
+      
+  def card_registration
+    @card = Card.find(params[:id])
+  end
 
-    def edit 
-    end
-        
-    def card_registration
-      @card = Card.find(params[:id])
-    end
+  def add_card_registration
+  end
 
-    def add_card_registration
-    end
+  def card_information
+  end
 
-    def card_information
-    end
+  def identification
+    @user = User.find(params[:id])
+    birth_year = @user.birth_year.to_s
+    birth_month = @user.birth_month.to_s
+    birth_day = @user.birth_day.to_s
+    @birth= birth_year + "/" + birth_month + "/" + birth_day
+  end
 
-    def identification
-      @user = User.find(params[:id])
-      birth_year = @user.birth_year.to_s
-      birth_month = @user.birth_month.to_s
-      birth_day = @user.birth_day.to_s
-      @birth= birth_year + "/" + birth_month + "/" + birth_day
-    end
-
-    def buy
-    end
-    
-    def sell
-        @item = Item.new
-        @item.images.build
-        @category_parent_array = ["---"]
-        Category.where(ancestry: nil).each do |parent|
-         @category_parent_array << parent.name
-        end
-    end
-
-    def get_category_children
-        #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-        @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-     end
+  def buy
+  end
   
-     # 子カテゴリーが選択された後に動くアクション
-    def get_category_grandchildren
-        #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-        @category_grandchildren = Category.find( "#{params[:child_id]}").children
+  def sell
+    @item = Item.new
+    @item.images.build
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+  end
+
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
     end
 
-    def create
-        @item = Item.new(item_params)
-        if @item.save
-            params[:images][:photo].each do |photo|
-                @item.images.create(photo: photo, item_id: @item.id)
-            end
-            redirect_to root_path
-        else
-            redirect_to sell_items_path
-        end
-    end
+    # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find( "#{params[:child_id]}").children
+  end
 
-    def detail
-        @item = Item.find(30)
-        @images = Image.find(24)
-        @image = @images.photo
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      params[:images][:photo].each do |photo|
+        @item.images.create(photo: photo, item_id: @item.id)
+      end
+      redirect_to root_path
+    else
+      redirect_to sell_items_path
     end
-    
-    def card_edit
-        @card = Card.new
-    end
+  end
 
-    def card_upload
-        @card = Card.create(card_params)
-    end
+  def detail
+    @item = Item.find(30)
+    @images = Image.find(24)
+    @image = @images.photo
+  end
+  
+  def card_edit
+    @card = Card.new
+  end
 
-    private
+  def card_upload
+    @card = Card.create(card_params)
+  end
 
-    def item_params
-        params.require(:item).permit(:name, :detail, :category_id, :brand, :condition, :delivery, :days, :area, :price, images_attributes: [:photo]).merge(user_id: current_user.id)
-    end
+  private
 
-    def image_params
-        params.require(:image).permit(:photo)
-    end
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :brand, :condition, :delivery, :days, :area, :price, images_attributes: [:photo]).merge(user_id: current_user.id)
+  end
 
-    def card_params
-        params.require(:card).permit(:card_number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id)
-    end
+  def image_params
+    params.require(:image).permit(:photo)
+  end
+
+  def card_params
+    params.require(:card).permit(:card_number, :exp_month, :exp_year, :cvc).merge(user_id: current_user.id)
+  end
 
 end
