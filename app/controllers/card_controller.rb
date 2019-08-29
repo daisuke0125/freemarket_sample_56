3d3aaf2805ef96007db9870360da523654caae8e
KotaMorikawa <koke.kokota@gmail.com>
2019-08-29 11:55:45 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,13 +8,6 @@ class CardController < ApplicationController
 
   end
 
-  # def edit
-  #   card = Card.where(user_id: current_user.id)
-  #   redirect_to action: "show" if card.exists?
-  # end
-
-
-
   def delete 
     card = Card.where(user_id: current_user.id).first
     if card.blank?
@@ -26,31 +19,6 @@ class CardController < ApplicationController
     end
       redirect_to action: "new"
   end
-
-  # def show 
-  #   Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-  #   # if params['payjp-token'].blank?
-  #   #   redirect_to action: "new"
-  #   # else
-  #     customer = Payjp::Customer.create(
-  #     description: '登録テスト', #なくてもOK
-  #     # email: current_user.email, #なくてもOK
-  #     card: params['payjp-token'],
-  #     metadata: {user_id: current_user.id}
-  #     ) #念の為metadataにuser_idを入れましたがなくてもOK
-  #     @card = Card.new(
-  #       user_id: current_user.id, 
-  #       customer_id: customer.id, 
-  #       card_id: customer.default_card,
-  #     )
-  #     if @card.save
-  #       redirect_to card_registration_item_path
-  #       # action: "show"
-  #     else
-  #       redirect_to action: "pay"
-  #     end
-  #   # end
-  # end
 
   def show 
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
